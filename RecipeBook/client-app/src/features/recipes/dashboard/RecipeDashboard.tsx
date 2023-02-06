e7169855aeb8412019d4512a336eb14bfdb17b9b
CoconutAlex <aakarydas@gmail.com>
2023-02-06 16:16:29 +0200

    Loader before Retrieve Recipies

--- RecipeBook/client-app/src/features/recipes/dashboard/RecipeDashboard.tsx
+++ RecipeBook/client-app/src/features/recipes/dashboard/RecipeDashboard.tsx
@@ -6,14 +6,25 @@ import RecipeList from "./RecipeList";
 
 export default function RecipeDashboard() {
 
+    const [isLoading, setIsLoading] = useState(false);
     const [recipes, setRecipes] = useState<Recipe[]>([]);
 
     useEffect(() => {
+        setIsLoading(true);
         axios.get<Recipe[]>('http://localhost:5118/Recipe/GetAllRecipes')
             .then(response => {
                 setRecipes(response.data);
+                setIsLoading(false);
             })
     }, [])
+
+    if (isLoading) {
+        return (
+            <div className="loader">
+                <div className="spinner"></div>
+            </div>
+        );
+    }
 
     return (
         <Grid>
